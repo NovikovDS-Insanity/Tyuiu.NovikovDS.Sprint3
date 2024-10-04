b484e67757f49d0e6272464f846997cf53adb409
NovikovDS-Insanity <Tyuiu.NovikovDS@outlook.com>
2024-10-04 18:05:21 +0300

    Add project files.

---
+++ Tyuiu.NovikovDS.Sprint3.Task0.V3/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.NovikovDS.Sprint3.Task0.V3.Lib;
+
+DataService ds = new();
+
+Console.Title = "Спринт #1 | Выполнил: Новиков Д. С. | РППб-24-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #6                                                              *");
+Console.WriteLine("* Выполнил: Новиков Д. С. | РППб-24-1                                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+Console.WriteLine("* по формуле.                                                             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите значение X:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.GetSumSeries(x, y));
+Console.ReadLine();
