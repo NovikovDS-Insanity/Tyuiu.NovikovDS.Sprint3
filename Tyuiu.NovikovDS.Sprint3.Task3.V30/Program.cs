071c5197942eac61d92cffc4f1c79d0df7171442
NovikovDS-Insanity <Tyuiu.NovikovDS@outlook.com>
2024-10-04 19:54:44 +0300

    Task3

---
+++ Tyuiu.NovikovDS.Sprint3.Task3.V30/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.NovikovDS.Sprint3.Task3.V30.Lib;
+
+DataService ds = new();
+
+Console.Title = "Спринт #3 | Выполнил: Новиков Д. С. | РППб-24-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #30                                                             *");
+Console.WriteLine("* Выполнил: Новиков Д. С. | РППб-24-1                                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Используя цикл foreach подсчитать максимальное количество букв y,       *");
+Console.WriteLine("* находящихся на соседних позициях в строке: fyyklbtyn ygrc vfyyyyh.      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string value = "fyyklbtyn ygrc vfyyyyh";
+char chr = 'y';
+
+Console.WriteLine("Исходная строка - " + value);
+Console.WriteLine("Исходный символ - " + chr);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Максимальное повторение символа " + chr + ": " + ds.GetMaxCharCount(value, chr));
+Console.ReadLine();
